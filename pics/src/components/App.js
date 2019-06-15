a3a4dd286d17a52f00b4c2acb6176b26ba530870
JaewooNam <namssabu12@gmail.com>
2019-06-16 00:15:16 +0900

    API with axios

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
   // props go down. to search bar, image list
-  onSearchSubmit(term){
-    console.log(term)
+  onSearchSubmit(term) {
+    // GET
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term},
+      headers: {
+        Authorization: 'Client-ID e86b4b6249663f2210d6dc843c59dc0f4629ddb6cb4d4cfe2ae45725a4610f7d'
+      }
+    })
   }
   
   render() {
