cdb0fb81cb226daf9d5ad81528d14dd48992f812
JaewooNam <namssabu12@gmail.com>
2019-06-15 16:49:57 +0900

    Handling form submittal

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -4,22 +4,34 @@ class SearchBar extends React.Component {
   state = { term: '' }
   // onChange(changes text in input)
   // onSubmit (sumit a form)
-
   // Also handle is good for function name i.e) handleInputChange
-
-  
   // Controlled / Uncontrolled form element
+
+  /*
+  * < Controlled Elements >
+  * User types in input
+  * Callback gets invokes
+  * We call setstate with the new value
+  * Component rerenders
+  * Input is told what its value is (coming from state)
+  */
+
+  onFormSubmit(event) {
+    event.preventDefault()
+    
+    console.log(this.state.term) // error
+  }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
               value={this.state.term}
-              onChange={(e) => this.setState({ term: e.target.value })}
+              onChange={(e) => this.setState({ term: e.target.value.toUpperCase() })}
             />
             </div>
         </form>
