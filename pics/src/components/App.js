8c6158d9691476b32b81e9333ec2b63d80cd3aa4
JaewooNam <namssabu12@gmail.com>
2019-06-16 00:30:26 +0900

    Context issue occurs

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,25 +3,25 @@ import axios from 'axios'
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
-  // props go down. to search bar, image list
+  state = { images: [] }
+
   async onSearchSubmit(term) {
-    // GET
-    const response = await axios
-      .get('https://api.unsplash.com/search/photos', {
-        params: { query: term},
-        headers: {
-          Authorization: 'Client-ID e86b4b6249663f2210d6dc843c59dc0f4629ddb6cb4d4cfe2ae45725a4610f7d'
-        }
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term},
+      headers: {
+        Authorization: 'Client-ID e86b4b6249663f2210d6dc843c59dc0f4629ddb6cb4d4cfe2ae45725a4610f7d'
       }
-    )
+    })
     
-    console.log(response.data.results)
+    console.log('this: ', this)
+    this.setState({ images: response.data.results })
   }
   
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     )
   }
