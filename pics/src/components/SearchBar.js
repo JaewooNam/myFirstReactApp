387e6f09d31b0443a04813a9fe548670be5e7efd
JaewooNam <namssabu12@gmail.com>
2019-06-15 16:35:17 +0900

    Change uncontrolled to controlled elements

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,26 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+  state = { term: '' }
+  // onChange(changes text in input)
+  // onSubmit (sumit a form)
+
+  // Also handle is good for function name i.e) handleInputChange
+
+  
+  // Controlled / Uncontrolled form element
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-              <input type="text" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
             </div>
         </form>
       </div>
