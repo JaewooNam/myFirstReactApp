52e12e0b7497eac61597324b796fac41379a47e4
JaewooNam <namssabu12@gmail.com>
2019-06-15 23:35:31 +0900

    Invoke callback in Children

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import SearchBar from './SearchBar'
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '10px' }}>
-      <SearchBar />
-    </div>
-  )
+class App extends React.Component {
+  // props go down. to search bar, image list
+  onSearchSubmit(term){
+    console.log(term)
+  }
+  
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    )
+  }
 }
 
 export default App
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -2,27 +2,11 @@ import React from 'react'
 
 class SearchBar extends React.Component {
   state = { term: '' }
-  // onChange(changes text in input)
-  // onSubmit (sumit a form)
-  // Also handle is good for function name i.e) handleInputChange
-  // Controlled / Uncontrolled form element
-
-  /*
-  * < Controlled Elements >
-  * User types in input
-  * Callback gets invokes
-  * We call setstate with the new value
-  * Component rerenders
-  * Input is told what its value is (coming from state)
-  */
 
   onFormSubmit = (event) => {
     event.preventDefault()
-    // this is not equal search bar class
-    console.log(this.state.term) // error
-    // What is 'this' used for in a class?
-    // How is the value of 'this' determined in a function ?
-    // -> arrow  function can be solved
+
+    this.props.onSubmit(this.state.term)
   }
 
   render() {
