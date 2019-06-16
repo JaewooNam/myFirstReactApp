06f70a5950bdeeeea0d07c20177cf9b9d9bbf462
JaewooNam <namssabu12@gmail.com>
2019-06-16 19:05:25 +0900

    Create React-redux project

---
+++ songs/src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+/*
+ * < Redux >
+ * Reducers - song list reducer, selected song reducer
+ * : Redux store contains all of reducers
+ * Action creators - selected songs
+*/
+
+// < React-Redux project system >
+// /src
+//   /actions - contains files related to action creators
+//   /components - files related to components
+//   /reducers - files related to reducers
+//   index.js - Sets up both the react and redux sides of the app
+
+const App = () => {
+  return <div>App</div>
+}
+
+export default App
