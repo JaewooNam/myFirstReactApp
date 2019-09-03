d8c04e72e125756358398e521618b14dc2cdf34a
JaewooNam <namssabu12@gmail.com>
2019-09-03 21:32:00 +0900

    Refactor

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { fetchUser } from '../actions'
 
 class UserHeader extends React.Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId)
-  }
-
   render() {
     const { user } = this.props
     if (!user) {
@@ -22,6 +17,5 @@ const mapStateToProps = (state, ownProps) => {
 }
 
 export default connect(
-  mapStateToProps,
-  { fetchUser }
+  mapStateToProps
 )(UserHeader)
